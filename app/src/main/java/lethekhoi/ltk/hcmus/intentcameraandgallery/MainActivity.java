5653af49b1a484678b32cc576bd95a0c68732caa
lethekhoi <lethekhoi@gmail.com>
2019-11-28 21:28:57 +0700

    camera and gallery layout

--- app/src/main/java/lethekhoi/ltk/hcmus/intentcameraandgallery/MainActivity.java
+++ app/src/main/java/lethekhoi/ltk/hcmus/intentcameraandgallery/MainActivity.java
@@ -1,14 +1,34 @@
 package lethekhoi.ltk.hcmus.intentcameraandgallery;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnCamera, btnGallery;
+    ImageView img;
+    int REQUEST_CODE_CAMERA = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnCamera = findViewById(R.id.buttonCamera);
+        btnGallery = findViewById(R.id.buttonGallery);
+        img = findViewById(R.id.imageview);
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.CAMERA},
+                        REQUEST_CODE_CAMERA
+                );
+            }
+        });
     }
 }
