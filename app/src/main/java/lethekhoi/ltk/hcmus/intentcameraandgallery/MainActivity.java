3efbbcd13ed731947ff248701d8be76f34d02e98
lethekhoi <lethekhoi@gmail.com>
2019-11-30 20:21:31 +0700

    capture image

--- app/src/main/java/lethekhoi/ltk/hcmus/intentcameraandgallery/MainActivity.java
+++ app/src/main/java/lethekhoi/ltk/hcmus/intentcameraandgallery/MainActivity.java
@@ -1,11 +1,17 @@
 package lethekhoi.ltk.hcmus.intentcameraandgallery;
 
 import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -30,5 +36,30 @@ public class MainActivity extends AppCompatActivity {
                 );
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_CAMERA) {
+
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //Action : chỉ thị
+                Intent intent = new Intent();
+                intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, REQUEST_CODE_CAMERA);
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_CODE_CAMERA && resultCode == RESULT_OK && data != null) {
+            //Log.d("BBB", data.getExtras().get("data").toString());
+            Bitmap Bitmap = (Bitmap) data.getExtras().get("data");
+            img.setImageBitmap(Bitmap);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+
     }
 }
